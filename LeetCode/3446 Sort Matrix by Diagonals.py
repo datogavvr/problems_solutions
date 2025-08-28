5babff8ddac5479813395245768185496404ea34
datogavr <dato.gavrilin@yandex.ru>
2025-08-28 16:30:54 +0300

    3446 problem lc

---
+++ LeetCode/3446 Sort Matrix by Diagonals.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        new_grid = [i for i in grid]
+        for i in range(n):
+            ind = [i, 0]
+            lt = [] # диагональ левого треугольника
+            rt = [] # диагональ правого треугольника
+
+            while ind[0] < n: # заполняем диагонали
+                lt.append(grid[ind[0]][ind[1]])
+                if i > 0:
+                    rt.append(grid[ind[1]][ind[0]])
+                ind[0] += 1
+                ind[1] += 1
+            lt.sort(reverse=1)
+            rt.sort()
+
+            ind = [i, 0]
+            while ind[0] < n: # добавляем диагонали в матрицу
+                new_grid[ind[0]][ind[1]] = lt[ind[1]]
+                if i > 0:
+                    new_grid[ind[1]][ind[0]] = rt[ind[1]]
+                ind[0] += 1
+                ind[1] += 1
+        return new_grid
+
+
+
