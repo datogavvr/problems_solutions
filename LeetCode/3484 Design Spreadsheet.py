1bf5295183bf4ff9f5b01f0892c454b92a2fcf4a
datogavr <dato.gavrilin@yandex.ru>
2025-09-23 13:20:19 +0300

    3484 problem lc

--- LeetCode/3484 Design Spreadsheet.py
+++ LeetCode/3484 Design Spreadsheet.py
@@ -1,20 +1,24 @@
 class Spreadsheet:
     def __init__(self, rows: int):
-        self.spreadsheet = {chr(i): [0 for j in range(rows)] for i in range(65, 91)}
-
-    def getCell(self, cell: str) -> int:
-        return self.spreadsheet[cell[0]][int(cell[1:])-1]
+        self.spreadsheet = {chr(i): {} for i in range(65, 91)}
 
     def setCell(self, cell: str, value: int) -> None:
-        self.spreadsheet[cell[0]][int(cell[1:])-1] = value
+        self.spreadsheet[cell[0]][int(cell[1:])] = value
 
     def resetCell(self, cell: str) -> None:
-        self.spreadsheet[cell[0]][int(cell[1:])-1] = 0
+        if int(cell[1:]) in self.spreadsheet[cell[0]]:
+            del self.spreadsheet[cell[0]][int(cell[1:])]
 
     def getValue(self, formula: str) -> int:
         a, b = map(str, formula[1:].split('+'))
         if not a.isdigit():
-            a = self.getCell(a)
+            if int(a[1:]) in self.spreadsheet[a[0]]:
+                a = self.spreadsheet[a[0]][int(a[1:])]
+            else:
+                a = 0
         if not b.isdigit():
-            b = self.getCell(b)
+            if int(b[1:]) in self.spreadsheet[b[0]]:
+                b = self.spreadsheet[b[0]][int(b[1:])]
+            else:
+                b = 0
         return int(a)+int(b)
